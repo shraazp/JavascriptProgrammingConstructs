cc792c9a4304d5c85433a91e4104dbefe7b097af
shravya p <48832081+shraazp@users.noreply.github.com>
2021-09-29 19:55:12 +0530

    refactored:program to sort and find second min and max

--- RandomArrayWithoutSort.js
+++ RandomArrayWithoutSort.js
@@ -1,18 +1,18 @@
 let randomNumber=new Array();
-for(var i=0;i<10;i++){
+for(let i=0;i<10;i++){
     //to get 3 digit random number
     randomNumber.push(Math.floor(Math.random()*(999-100+1)+100))
 }
 console.log(randomNumber)
-var arr=randomNumber
+let arr=randomNumber
 // get the max of the array
-var max = Math.max.apply(null, arr); 
+let max = Math.max.apply(null, arr); 
 //remove the maximum element
 arr.splice(arr.indexOf(max), 1); 
 //print the second largest
 console.log(Math.max.apply(null, arr))
 //get the min of the array
-var min=Math.min.apply(null,arr)
+let min=Math.min.apply(null,arr)
 //remove the min of the array
 arr.splice(arr.indexOf(min))
 //print the second smallest
